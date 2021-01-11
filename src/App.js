e9e52dd2b8c6d3f7ec8da1107f36535e7634a890
rupamswain1 <rupamswain1@gmail.com>
2021-01-12 00:11:23 +0530

    create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      pocketMonsters: [
+        // {
+        //   name:"Pikachu",
+        //   id:1
+        // },
+        // {
+        //   name:"Bulbasaur",
+        //   id:2
+        // },
+        // {
+        //   name:"Charmander",
+        //   id:3
+        // },
+        // {
+        //   name:"Squirtel",
+        //   id:4
+        // },
+      ]
+    };
+  }
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users").
+    then(response=>response.json()).
+    then(users=>this.setState({pocketMonsters:users}));
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            this.state.pocketMonsters.map(pocketMonsters=><h3 key={pocketMonsters.id}>{pocketMonsters.name}</h3>)
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
